21d5c7aa0fd8b3d2e3a2ce4fe00e71e6a0f0dbfc
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-03-28 18:50:01 +0300

    Initial Commit

---
+++ KousaBLC.js
@@ -0,0 +1,9 @@
+const BLC = require(__dirname + '/../blc');
+  const slc = require(__dirname + '/../../slc');
+
+  module.exports = class KousaBLC extends BLC {
+    process(input){
+      return new Promise((resolve, reject) => {
+      });
+    }
+  };
---
+++ index.js
@@ -0,0 +1,32 @@
+const program = require('commander'),
+  fs = require('fs')
+
+const BLC = name => {
+  return `const BLC = require(__dirname + '/../blc');
+  const slc = require(__dirname + '/../../slc');
+
+  module.exports = class ${name}BLC extends BLC {
+    process(input){
+      return new Promise((resolve, reject) => {
+      });
+    }
+  };`
+}
+const createBLC = name => {
+  const file = fs.createWriteStream(`${name}BLC.js`);
+  file.on('error', err => { throw new Error(`Error: ${err}`) })
+  file.write(BLC(name))
+  file.end()
+}
+
+program
+  .version('1.2.0')
+  .description('A tool to manage the environment variables of an Elastic Beanstalk App')
+  .option('-c, --create', 'create BLC')
+  .option('-n, --name [name]', 'name of the Elastic Beanstalk environment to update')
+  .parse(process.argv)
+
+if(program.create) {
+  createBLC(program.name)
+}
+
